970851c5c855b5b8bac97dcb141fc3fc004024e8
BertrandConxy <90222110+BertrandConxy@users.noreply.github.com>
2022-01-23 13:11:33 +0200

    created account registration and log in form

--- index.js
+++ index.js
@@ -14,3 +14,36 @@ closeMenu.addEventListener('click', ()=>{
     },500)
     
 })
+
+//  Registration form 
+const user = document.getElementById('account-btn')
+const closeForm = document.getElementById('close-form')
+user.addEventListener('click', ()=>{
+    document.querySelector('.account-form').classList.add('active-form')
+});
+
+closeForm.addEventListener('click', ()=>{
+    closeForm.classList.toggle('transform')
+    setTimeout( ()=> {
+        document.querySelector('.account-form').classList.remove('active-form')
+    },500)
+    
+})
+const loginBtn = document.querySelector('.log-in-btn')
+const registerBtn = document.querySelector('.register-btn')
+
+registerBtn.addEventListener('click', ()=>{
+    registerBtn.classList.add('active')
+    loginBtn.classList.remove('active')
+    document.querySelector('.login-form').classList.remove('active')
+    document.querySelector('.register-form').classList.add('active')
+
+})
+
+loginBtn.addEventListener('click', ()=>{
+    loginBtn.classList.add('active')
+    registerBtn.classList.remove('active')
+    document.querySelector('.login-form').classList.add('active')
+    document.querySelector('.register-form').classList.remove('active')
+
+})
